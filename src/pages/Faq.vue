8ea44a62d0934aeb84abb2a16912636bb87b6a97
Alex Vieira <alex.p.vieira@gmail.com>
2021-07-19 18:53:42 -0300

    Updating FAQ page

--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -34,20 +34,20 @@ export default {
     data() {
         return {
             faqs: [{
-                answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facilis sed inventore, perspiciatis dicta quibusdam voluptatibus perferendis voluptas deserunt, illo praesentium porro iure esse minima consequuntur similique iste fugiat assumenda!',
-                question: 'Lorem ipsum dolor sit amet dolor sit amet?'
+                answer: 'O principal objetivo do app é angariar fundos para doação através de empresas privadas parceiras e doar esse dinheiro para ONGs que tenham interesse em se tornar parceiras do app.',
+                question: 'Qual o objetivo do app?'
             }, {
-                answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facilis sed inventore, perspiciatis dicta quibusdam voluptatibus perferendis voluptas deserunt, illo praesentium porro iure esse minima consequuntur similique iste fugiat assumenda!',
-                question: 'Lorem ipsum dolor sit amet ipsum dolor ipsum dolor ipsum dolor?'
+                answer: 'Aqui as empresas com fins lucrativos se tornam parceiras e pagam valores para divulgar atividades relacionadas com o mundo das ONGs, a cada visualização recebida em um texto e/ou vídeo essa empresa vai realizar uma doação para um fundo de doação, ao longo de um mês juntamos todas as doações feitas e doamos o valor diretamente para as ONGs parceiras dividindo o valor total igualmente entre todas.',
+                question: 'Como as ONGs ganham dinheiro se eu não faço nenhuma doação?'
             }, {
-                answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facilis sed inventore, perspiciatis dicta quibusdam voluptatibus perferendis voluptas deserunt, illo praesentium porro iure esse minima consequuntur similique iste fugiat assumenda!',
-                question: 'Lorem ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet?'
+                answer: 'Através do App apenas empresas privadas poderão realizar doações mediante as visualizações de suas publicações, se você tem interesse de divulgar o trabalho social da sua empresa e realizar uma doação para isso, entre em contato com o nosso SAC. Agora se você deseja apenas realizar uma doação para uma ONG como pessoa física, então você pode olhar nossa lista de ONGs parceiras e entrar em contato diretamente com uma da sua escolha para realizar a doação direta, temos certeza de que todas ficarão felizes em contar com sua ajuda financeira e/ou de trabalho voluntário.',
+                question: 'Posso realizar doações para as ONGs como pessoa física?'
             }, {
-                answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facilis sed inventore, perspiciatis dicta quibusdam voluptatibus perferendis voluptas deserunt, illo praesentium porro iure esse minima consequuntur similique iste fugiat assumenda!',
-                question: 'Lorem ipsum dolor sit amet amet amet?'
+                answer: 'Para ganhar pontos basta ler ou assistir aos vídeos publicados até o final e quando aparecer o botão de concluir leitura, automaticamente você ganhará os pontos na sua conta e poderá subir de ranking.',
+                question: 'Como eu ganho pontos no ranking do app?'
             }, {
-                answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facilis sed inventore, perspiciatis dicta quibusdam voluptatibus perferendis voluptas deserunt, illo praesentium porro iure esse minima consequuntur similique iste fugiat assumenda!',
-                question: 'Lorem ipsum dolor sit amet dolor sit dolor sit?'
+                answer: 'No menu do app você pode clicar em perfil você poderá alterar suas informações pessoais como e-mail e senha.',
+                question: 'Como faço para atualizar minhas informações?'
             }]
         }
     }
